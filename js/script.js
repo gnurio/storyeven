90ca14b98865895b7f9ba80b675e4b6d83d2fe54
George Nurijanian <george.nuri@gmail.com>
2020-07-17 23:09:03 +1200

    Started working with jQuery to grab the text from textarea

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@ ctx.lineTo(400,0);
 ctx.lineWidth = 10;
 ctx.strokeStyle = '#747e86';
 ctx.stroke();
+
+
+
+let area = document.getElementById("big-text");             
+let lines = area.value.replace(/\r\n/g,"\n").split("\n");
