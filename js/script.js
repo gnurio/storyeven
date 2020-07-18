205b9a5d9e3e2b34eca5172314c69b0801f69ad9
George Nurijanian <george.nuri@gmail.com>
2020-07-18 13:50:32 +1200

    Connected submit button to form and added some basic word tokenization

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 let c = document.getElementById('the-line');
 let ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.lineTo(400,0);
+ctx.moveTo(0, 0);
+ctx.lineTo(400, 0);
 ctx.lineWidth = 10;
 ctx.strokeStyle = '#747e86';
 ctx.stroke();
 
-
-
-let area = document.getElementById("big-text");             
-let lines = area.value.replace(/\r\n/g,"\n").split("\n");
+$(document).ready(function () {
+    $("#target").submit(function () {
+        let area = $("#big-text").val();
+        let words = area.split(" ");
+        let length = words.length;
+        alert(`The text has ${length} words and contains these words: ${words}`);
+    });
+})
