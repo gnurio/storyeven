f008bc468bc077345f5e742f0b9f9416e2520133
George Nurijanian <george.nuri@gmail.com>
2020-07-19 14:12:33 +1200

    Split JS but not working properly. Added Voca for word processing, but I don't think it works

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let c = document.getElementById('the-line');
-let ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(400, 0);
-ctx.lineWidth = 10;
-ctx.strokeStyle = '#747e86';
-ctx.stroke();
+import 'line';
+import "../line";
+import { drawLine } from '../line';
+import voca from 'voca';
+const words = require('voca/words');
+
+drawLine();
 
 $(document).ready(function () {
     $("#target").submit(function () {
         let area = $("#big-text").val();
-        let words = area.split(" ");
-        let length = words.length;
-        alert(`The text has ${length} words and contains these words: ${words}`);
+        let wordSplit = words(area);
+        let length = wordSplit.length;
+        alert(`The text has ${length} words and contains these words: ${wordSplit}`);
     });
 })
+
