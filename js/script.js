65b478f80a68c6c2f1ea32fcea154d163d4e9366
George Nurijanian <george.nuri@gmail.com>
2020-07-22 20:18:04 +1200

    Rollback to old version

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-import MarkdownIt from 'markdown-it';
-var md = new MarkdownIt();
+function drawLine() {
+
+    let c = document.getElementById('the-line');
+    let ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(400, 0);
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = '#747e86';
+    ctx.stroke();
+}
+
 
 
 drawLine();
@@ -19,14 +29,3 @@ $(document).ready(function () {
 
 })
 
-function drawLine() {
-
-    let c = document.getElementById('the-line');
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(400, 0);
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = '#747e86';
-    ctx.stroke();
-}
