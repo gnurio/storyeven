c0549e0addbbf4ba0787f7c10f6997085c987670
George Nurijanian <george.nuri@gmail.com>
2020-08-15 12:27:01 +1200

    Last try to get require running

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 "use strict";
 
-import MarkdownIt from './markdown-it';
-var md = new MarkdownIt(); // trying to use Markdown-it here. Before I used the CDN link in HTML and that worked.
-
-import Tokenizer from 'tokenize-text';
-var tokenize = new Tokenizer(); // but Tokenize-text doesn't have a link I can paste in HTML, so I need to use it here
+const MarkdownIt = require('markdown-it'), md = new MarkdownIt();// trying to use Markdown-it here. Before I used the CDN link in HTML and that worked.
+const Tokenizer = require('tokenize-text'), tokenize = new Tokenizer(); // but Tokenize-text doesn't have a link I can paste in HTML, so I need to use it here
 
 
 const drawLine = () => {
